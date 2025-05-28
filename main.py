72c4532e845fa2ae6c60f61689861af940876b72
AlgoClassWork <kyxec.play@gmail.com>
2025-05-28 12:26:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import database
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
 
@@ -13,7 +13,15 @@ def home():
 @app.route('/start-quiz', methods=['POST'])
 def start_quiz():
     """Начинает викторину: загружает вопросы, инициализирует сессию."""
-    pass
+    all_question_from_db = database.get_all_questions()
+    if not all_question_from_db:
+        return redirect('/')
+    
+    session['questions'] = [dict(question) for question in all_question_from_db]
+    session['current_question_index'] = 0
+    session['score'] = 0
+
+    return redirect('/quiz')
 
 # --- Маршруты для простой админ-панели ---
 
